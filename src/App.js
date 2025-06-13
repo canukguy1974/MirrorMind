dcd8cde48ef832d80a6457d5e5fb7958cc48b928
canukguy1974 <canukguy1974@gmail.com>
2025-06-13 15:59:00 -0400

    Removed MirrorMindAgent to check functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import MirrorMindAgent from './components/mirrormind-agent';
 
 function App() {
   return (
-      <div className="App">
       <div style={{ background: 'midnightblue', color: 'white', height: '100vh' }}>
       <h1>MirrorMind – Client Render Test</h1>
       <p>If you see this, React works fine client-side.</p>
-      <MirrorMindAgent />
+      {/* <MirrorMindAgent /> */}
       <MirrorMindApp />
     </div>
   );
