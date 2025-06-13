c34bde996767e8be1c421bc269020a4f6e2cc778
canukguy1974 <canukguy1974@gmail.com>
2025-06-13 15:36:23 -0400

    Fixed server-side crash by checking window/document

--- src/components/mirrormind-agent.jsx
+++ src/components/mirrormind-agent.jsx
@@ -2,19 +2,21 @@ import { useEffect } from "react";
 
 export default function MirrorMindAgent() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.type = "module";
-    script.src = "https://agent.d-id.com/v1/index.js";
-    script.setAttribute("data-name", "did-agent");
-    script.setAttribute("data-mode", "fabio");
-    script.setAttribute(
-      "data-client-key",
-      "Z29vZ2xlLW9hdXRoMnwxMTU4NzQyMzg4ODY5MzYxMjMwMjM6R1dIdmFhNHRjanBsT0xnRmZnZDMx"
-    );
-    script.setAttribute("data-agent-id", "agt_pySexgPa");
-    script.setAttribute("data-monitor", "true");
+    if (typeof window !== "undefined") {
+      const script = document.createElement("script");
+      script.type = "module";
+      script.src = "https://agent.d-id.com/v1/index.js";
+      script.setAttribute("data-name", "did-agent");
+      script.setAttribute("data-mode", "fabio");
+      script.setAttribute(
+        "data-client-key",
+        "Z29vZ2xlLW9hdXRoMnwxMTU4NzQyMzg4ODY5MzYxMjMwMjM6R1dIdmFhNHRjanBsT0xnRmZnZDMx"
+      );
+      script.setAttribute("data-agent-id", "agt_pySexgPa");
+      script.setAttribute("data-monitor", "true");
 
-    document.body.appendChild(script);
+      document.body.appendChild(script);
+    }
   }, []);
 
   return null;
