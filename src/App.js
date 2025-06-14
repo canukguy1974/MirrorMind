617ffc362fa9a79b7cc5c2b3e8470e407ecb56ed
canukguy1974 <canukguy1974@gmail.com>
2025-06-13 20:44:55 -0400

    Changed snippet for D-ID

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import MirrorMindAgent from './components/mirrormind-agent';
 function App() {
   return (
       <div className="App">
-      <div style={{ background: 'midnightblue', color: 'white', height: '100vh' }}>
-      <h1>MirrorMind – Client Render Test</h1>
-      <p>If you see this, React works fine client-side.</p>
       <MirrorMindAgent />
       <MirrorMindApp />
     </div>
